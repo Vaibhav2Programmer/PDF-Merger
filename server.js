f153fc98b660b9f9357c6db3899480c896094946
Vaibhav Rajoriya <vaibhavrajoriya.21@gmail.com>
2022-12-31 20:02:46 +0530

    First Commit
    This is the initial Commit and I will continue to improve this website/project and will add more functionalities to it.

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const multer = require('multer')
+const {mergePDFs} = require('./merge')
+
+const upload = multer({dest: 'uploads/'})
+app.use('/static',express.static('public'))
+const port = 3000
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,"Template/index.html"))
+}) 
+
+app.post('/merge',upload.array('pdfs',2), async(req, res, next)=>{
+    console.log(req.files)
+    let d = await mergePDFs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path)) // Merging Files 1 and 2
+    res.redirect(`http://localhost:3000/static/${d}.pdf`)
+
+  //  res.send({data: req.files})
+    //req.files is array of photo files
+    //req.body will contain the text fields, if there were only
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`)
+})
